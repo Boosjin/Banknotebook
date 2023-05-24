8b011c153160022162a24754a0d024e0f003ecde
Kourosh Khosravi <kourosh.khosravi.boosjin@gmail.com>
2023-05-24 12:24:00 +0330

    add FileProgress Entity

---
+++ src/main/java/com/tosan/entity/FileProgress.java
@@ -0,0 +1,45 @@
+package com.tosan.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "FILE_PROGRESS")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class FileProgress {
+
+    @Id
+    @Column(name = "FILE_LOCATION")
+    @NotBlank(message = "File Location Can Not Be An Empty String")
+    private String fileLocation;
+
+    @Column(name = "PROCESSED_CHARACTERS")
+    @PositiveOrZero(message = "Number Of Processed Characters Can Not Be Negative")
+    private Long processedCharacters;
+
+    @Column(name = "VALID_RECORDS")
+    @PositiveOrZero(message = "Number Of Valid Records Can Not Be Negative")
+    private Integer validRecords;
+
+    @Column(name = "INVALID_RECORDS")
+    @PositiveOrZero(message = "Number Of Invalid Records Can Not Be Negative")
+    private Integer invalidRecords;
+
+    @Column(name = "FINISHED")
+    private Boolean finished;
+}
