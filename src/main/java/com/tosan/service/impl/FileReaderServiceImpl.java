3e56151b62639787e4d5edca05cb7229e3e7ea8f
Kourosh Khosravi <kourosh.khosravi.boosjin@gmail.com>
2023-05-24 16:06:09 +0330

    made FileReaderServiceImpl better

--- src/main/java/com/tosan/service/impl/FileReaderServiceImpl.java
+++ src/main/java/com/tosan/service/impl/FileReaderServiceImpl.java
@@ -11,11 +11,24 @@ public class FileReaderServiceImpl implements FileReaderService {
 
     @Override
     public FileReader getFileReader(String fileUrl) {
+
+        if (fileUrl == null) {
+            log.info("You Can Not Provide A Null String As File Location\n\n\n");
+            System.exit(0);
+        }
+
+        if (fileUrl.trim().equals("")) {
+            log.info("You Can Not Provide An Empty String As File Location\n\n\n");
+            System.exit(0);
+        }
+
+        FileReader fileReader = null;
         try {
-            return new FileReader(fileUrl);
+            fileReader = new FileReader(fileUrl);
         } catch (FileNotFoundException e) {
             log.info("File Was Not Found\nProvided Location: " + fileUrl);
-            throw new RuntimeException(e);
+            System.exit(0);
         }
+        return fileReader;
     }
 }
