b921ea7d9385713260761834a396c233635d8ee9
Kourosh Khosravi Boosjin <kourosh.khosravi.boosjin@gmail.com>
2023-05-30 11:00:12 +0330

    add code for getting processed characters' ranges

--- src/main/java/com/tosan/service/FileProgressService.java
+++ src/main/java/com/tosan/service/FileProgressService.java
@@ -2,9 +2,12 @@ package com.tosan.service;
 
 import com.tosan.entity.FileProgress;
 
+import java.util.List;
+
 public interface FileProgressService {
     void saveFileProgress(FileProgress fileProgress);
     FileProgress getFileProgress(String fileUrl);
     boolean isFileCompletelyProcessed(String fileUrl);
     long getNumberOfProcessedCharacters(String fileUrl);
+    List<Long> getProcessedCharactersRanges(String fileUrl);
 }
--- src/main/java/com/tosan/service/impl/FileProgressServiceImpl.java
+++ src/main/java/com/tosan/service/impl/FileProgressServiceImpl.java
@@ -2,8 +2,14 @@ package com.tosan.service.impl;
 
 import com.tosan.dao.FileProgressDao;
 import com.tosan.entity.FileProgress;
+import com.tosan.entity.ProcessedCharactersRange;
 import com.tosan.service.FileProgressService;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 @Slf4j
 public class FileProgressServiceImpl implements FileProgressService {
@@ -38,15 +44,42 @@ public class FileProgressServiceImpl implements FileProgressService {
 
     @Override
     public boolean isFileCompletelyProcessed(String fileUrl) {
-        FileProgress fileProgress = this.getFileProgress(fileUrl);
+        final FileProgress fileProgress = this.getFileProgress(fileUrl);
         if (fileProgress == null) return false;
         else return fileProgress.getFinished();
     }
 
     @Override
     public long getNumberOfProcessedCharacters(String fileUrl) {
-        FileProgress fileProgress = this.getFileProgress(fileUrl);
+        final FileProgress fileProgress = this.getFileProgress(fileUrl);
         if (fileProgress == null) return 0;
         else return fileProgress.getNumberOfProcessedCharacters();
+    }
+
+    @Override
+    public List<Long> getProcessedCharactersRanges(String fileUrl) {
+        // getting and sorting ranges
+        final FileProgress fileProgress = this.getFileProgress(fileUrl);
+        final List<ProcessedCharactersRange> processedCharactersRanges = fileProgress.getProcessedCharactersRanges();
+        List<Long> ranges = new LinkedList<>();
+        processedCharactersRanges.forEach(processedCharactersRange ->
+        {
+            ranges.add(processedCharactersRange.getBeginning());
+            ranges.add(processedCharactersRange.getEnd());
+        });
+        if (ranges.size() > 2) {
+            // remove neighbour ranges
+            Collections.sort(ranges);
+            List<Long> mergedRanges = new ArrayList<>();
+            mergedRanges.add(ranges.get(0));
+            for (int i = 1; i < ranges.size() - 1; i += 2) {
+                if (ranges.get(i) + 1 == ranges.get(i + 1)) continue;
+                mergedRanges.add(ranges.get(i));
+                mergedRanges.add(ranges.get(i + 1));
+            }
+            mergedRanges.add(ranges.get(ranges.size() - 1));
+            return mergedRanges;
+        }
+        return ranges;
     }
 }
