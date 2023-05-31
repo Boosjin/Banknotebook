275822e17841cf554244fe0dc056b0966fa563ee
Kourosh Khosravi Boosjin <kourosh.khosravi.boosjin@gmail.com>
2023-05-31 19:27:49 +0330

    add code for calling csv service and starting the saving progress

--- src/main/java/com/tosan/Main.java
+++ src/main/java/com/tosan/Main.java
@@ -1,7 +1,22 @@
 package com.tosan;
 
+import com.tosan.dao.impl.FileProgressDaoImpl;
+import com.tosan.service.CsvService;
+import com.tosan.service.impl.CsvFileServiceImpl;
+import com.tosan.service.impl.CsvServiceImpl;
+import com.tosan.service.impl.FileProgressServiceImpl;
+import com.tosan.service.impl.FileReaderServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        CsvService csvService = new CsvServiceImpl(
+                new CsvFileServiceImpl(new FileReaderServiceImpl()),
+                new FileProgressServiceImpl(new FileProgressDaoImpl()),
+                new FileReaderServiceImpl());
+        csvService.beginSavingProgress();
     }
 }
