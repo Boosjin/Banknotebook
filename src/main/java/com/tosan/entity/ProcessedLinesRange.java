1c0c62a3f789691ef2a0be2d030e0c9dba3be048
Kourosh Khosravi Boosjin <kourosh.khosravi.boosjin@gmail.com>
2023-05-31 15:58:41 +0330

    add ProcessedLinesRange entity

---
+++ src/main/java/com/tosan/entity/ProcessedLinesRange.java
@@ -0,0 +1,47 @@
+package com.tosan.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "PROCESSED_LINES_RANGE")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@IdClass(ProcessedLinesRangeCompositeKey.class)
+public class ProcessedLinesRange {
+
+    @Id
+    @Column(name = "FILE_URL")
+    @NotBlank(message = "File URL Can Not Be An Empty String")
+    private String fileUrl;
+
+    @Id
+    @Column(name = "BEGINNING")
+    @NotNull(message = "The Beginning Of The Range Can Not Be Null")
+    @PositiveOrZero(message = "The Beginning Of The Range Can Not Be A Negative Number")
+    private Long beginning;
+
+    @Id
+    @Column(name = "END")
+    @NotNull(message = "The End Of The Range Can Not Be Null")
+    @PositiveOrZero(message = "The End Of The Range Can Not Be A Negative Number")
+    private Long end;
+}
+
+@Getter
+@Setter
+@EqualsAndHashCode
+class ProcessedLinesRangeCompositeKey implements Serializable {
+    private String fileUrl;
+    private Long beginning;
+    private Long end;
+}
