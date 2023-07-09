8f643488a5f8eb57d24ff918e827029520339330
Kourosh Khosravi Boosjin <kourosh.khosravi.boosjin@gmail.com>
2023-07-09 13:57:21 +0330

    add minor changes to Main class

--- src/main/java/com/tosan/Main.java
+++ src/main/java/com/tosan/Main.java
@@ -1,22 +1,25 @@
 package com.tosan;
 
 import com.tosan.dao.impl.FileProgressDaoImpl;
+import com.tosan.dao.impl.PersonDaoImpl;
 import com.tosan.service.CsvService;
-import com.tosan.service.impl.CsvFileServiceImpl;
-import com.tosan.service.impl.CsvServiceImpl;
-import com.tosan.service.impl.FileProgressServiceImpl;
-import com.tosan.service.impl.FileReaderServiceImpl;
+import com.tosan.service.impl.*;
 import lombok.extern.slf4j.Slf4j;
-
-import java.io.IOException;
 
 @Slf4j
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        // TODO add comments
+        // TODO put saving people an processed record numbers in one transaction
+        // TODO correct log levels
+        // TODO add validator for file url
         CsvService csvService = new CsvServiceImpl(
                 new CsvFileServiceImpl(new FileReaderServiceImpl()),
                 new FileProgressServiceImpl(new FileProgressDaoImpl()),
-                new FileReaderServiceImpl());
+                new FileReaderServiceImpl(),
+                new PersonServiceImpl(new PersonDaoImpl()));
+        csvService.fileCompletionCheck();
+        csvService.emptyFileCheck();
         csvService.beginSavingProgress();
     }
 }
